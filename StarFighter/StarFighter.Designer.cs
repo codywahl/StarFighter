7e0a1387d7432cd272a6306cc33a8dd317b36d20
Cody Wahl <Cody.Wahl@greenwayhealth.com>
2015-06-10 11:59:44 -0400

    Initial commit with the StarFighter solution added to the repo.

---
+++ StarFighter/StarFighter.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace StarFighter
+{
+    partial class StarFighter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.playground = new System.Windows.Forms.Panel();
+            this.ship = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.playground.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ship)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playground
+            // 
+            this.playground.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.playground.Controls.Add(this.ship);
+            this.playground.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.playground.Location = new System.Drawing.Point(0, 0);
+            this.playground.Name = "playground";
+            this.playground.Size = new System.Drawing.Size(515, 535);
+            this.playground.TabIndex = 0;
+            // 
+            // ship
+            // 
+            this.ship.Image = global::StarFighter.Properties.Resources.spiked_ship_3__small_blue_;
+            this.ship.Location = new System.Drawing.Point(185, 339);
+            this.ship.Name = "ship";
+            this.ship.Size = new System.Drawing.Size(155, 127);
+            this.ship.TabIndex = 2;
+            this.ship.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 535);
+            this.Controls.Add(this.playground);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.playground.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ship)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel playground;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox ship;
+    }
+}
+
---
+++ StarFighter/StarFighter.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace StarFighter
+{
+    public partial class StarFighter : Form
+    {
+        private const int NUMBER_OF_STARS = 30;
+        private Dictionary<PictureBox, int> starList = new Dictionary<PictureBox, int>();
+
+        public StarFighter()
+        {
+            InitializeComponent();
+
+            timer1.Enabled = true;
+            Cursor.Hide();
+
+            timer1.Interval = 50;
+            this.TopMost = true;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+
+            ship.Top = playground.Bottom - (playground.Bottom / 5);
+
+            MakeStars();
+        }
+
+        private void MakeStars()
+        {
+            Random r = new Random();
+            for (int i = 1; i <= NUMBER_OF_STARS; i++)
+            {
+                PictureBox pic = new PictureBox();
+
+                pic.Width = 5;
+                pic.Height = 5;
+                pic.BackColor = Color.White;
+                pic.Location = new Point(r.Next(playground.Width), r.Next(playground.Height));
+
+                starList.Add(pic, r.Next(15, 50));
+
+                playground.Controls.Add(pic);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ship.Left = Cursor.Position.X - (ship.Width / 2);
+
+            MoveStars();
+            MoveLasers();
+        }
+
+        private void ShootLaser()
+        {
+            PictureBox laser = new PictureBox();
+            laser.Width = 3;
+            laser.Height = 15;
+            laser.BackColor = Color.Red;
+            laser.Location = new Point(ship.Location.X + (ship.Width / 2), ship.Location.Y);
+
+            playground.Controls.Add(laser);
+        }
+
+        private void MoveLasers()
+        {
+            foreach (var cur in playground.Controls)
+            {
+                if (cur.GetType() == typeof(PictureBox))
+                {
+                    PictureBox aLaser = (PictureBox)cur;
+
+                    if (aLaser.Width == 3)
+                    {
+                        int index = playground.Controls.IndexOf((PictureBox)cur);
+                        int newY = playground.Controls[index].Location.Y - 35;
+
+                        if (newY < this.Top)
+                        {
+                            playground.Controls.RemoveAt(index);
+                        }
+                        else
+                        {
+                            playground.Controls[index].Location = new Point(playground.Controls[index].Location.X, newY);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void MoveStars()
+        {
+            foreach (var cur in playground.Controls)
+            {
+                if (cur.GetType() == typeof(PictureBox))
+                {
+                    PictureBox aStar = (PictureBox)cur;
+
+                    if (aStar.Width == 5)
+                    {
+                        int index = playground.Controls.IndexOf((PictureBox)cur);
+
+                        int newY = playground.Controls[index].Location.Y + starList[aStar];
+
+                        if (newY > this.Bottom)
+                        {
+                            newY = this.Top;
+                        }
+
+                        playground.Controls[index].Location = new Point(playground.Controls[index].Location.X, newY);
+                    }
+                }
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+
+            if (e.KeyCode == Keys.Space)
+            {
+                ShootLaser();
+            }
+        }
+    }
+}
